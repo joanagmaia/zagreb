984ed21e3357dd19944bc71bc3089fe0a1031192
powpillowpow <ssaracome@gmail.com>
2017-12-15 04:57:19 +0000

    edit

--- php/edit_album_form.php
+++ php/edit_album_form.php
@@ -9,12 +9,19 @@
    </div>
    <form action="../php/edit_album.php" method="GET" class="form-edit">
      <div class="inactive-wrapper">
-       <h3>Album inactive</h3>
-       <input type="checkbox" name="inactive_album" placeholder="Inactive album">
+       <h3>Album Available?</h3>
+       <select name="inactive_album" placeholder="Choose">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
      </div>
      <div class="price-wrapper">
        <h3>Price: </h3>
        <input type="number" name="price_album" value="<?php echo $price ?>">
+     </div>
+     <div class="price-wrapper">
+       <h3>Stock: </h3>
+       <input type="number" name="stock_album" value="<?php echo $stock ?>">
      </div>
      <input type="submit" class="light-blue-bg" value="Save" name="submit_edit">
    </form>
